6ba4070171ae18d960bd431366184d760ea5a03d
jelmerta <jelmer_alphenaar@hotmail.com>
2017-11-21 21:39:26 +0100

    still a mess

--- my_driver.py
+++ my_driver.py
@@ -9,3 +9,26 @@ class MyDriver(Driver):
     #     # Interesting stuff
     #     command = Command(...)
     #     return command
+
+    def drive(self, carstate: State) -> Command:
+        """
+        Produces driving command in response to newly received car state.
+
+        This is a dummy driving routine, very dumb and not really considering a
+        lot of inputs. But it will get the car (if not disturbed by other
+        drivers) successfully driven along the race track.
+        """
+
+        command = Command()
+        self.steer(carstate, 0.0, command)
+
+        # ACC_LATERAL_MAX = 6400 * 5
+        # v_x = min(80, math.sqrt(ACC_LATERAL_MAX / abs(command.steering)))
+        v_x = 80
+
+        self.accelerate(carstate, v_x, command)
+
+        if self.data_logger:
+            self.data_logger.log(carstate, command)
+
+        return command#trainNetwork(x, t):
